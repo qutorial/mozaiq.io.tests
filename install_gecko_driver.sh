bcf30dae7b8905eaba681e1813d32a2d46538522
Zaur Molotnikov <qutorial@gmail.com>
2018-11-23 10:41:46 +0100

    Driver update

--- install_gecko_driver.sh
+++ install_gecko_driver.sh
@@ -1,5 +1,6 @@
-ARCHIVE=geckodriver-v0.18.0-linux64.tar.gz
-wget https://github.com/mozilla/geckodriver/releases/download/v0.18.0/geckodriver-v0.18.0-linux64.tar.gz
+ARCHIVE="geckodriver-v0.23.0-linux64.tar.gz"
+URL="https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz"
+wget $URL
 tar -xzvf $ARCHIVE
 rm -f $ARCHIVE
 ./set_up_path.sh
